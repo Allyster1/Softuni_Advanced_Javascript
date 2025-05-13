68446a9445bea3b4d1146ab0f5ef7057616c6dde
Allyster <ovardovslav@gmail.com>
2025-05-14 00:27:20 +0300

    Update 02. Greatest-Common-Divisor-GCD.js

--- 01-Syntax-Functions-and-Statements-Exercises/02. Greatest-Common-Divisor-GCD.js
+++ 01-Syntax-Functions-and-Statements-Exercises/02. Greatest-Common-Divisor-GCD.js
@@ -1,1 +1,10 @@
-function greatestDivisor(firstNumber, secondNumber) {}
+function greatestDivisor(a, b) {
+  let min = a < b ? a : b;
+  for (; min > 0; min--) {
+    if (a % min === 0 && b % min === 0) {
+      console.log(min);
+      break;
+    }
+  }
+}
+greatestDivisor(15, 5);
