ddcd39ca3eb2213edea5f76e44d8f7c7091f1d7c
Allyster <ovardovslav@gmail.com>
2025-05-14 00:00:03 +0300

    Create 07. Cooking-by-Numbers.js

---
+++ 01-Syntax-Functions-and-Statements-Exercises/07. Cooking-by-Numbers.js
@@ -0,0 +1,21 @@
+function calculateOperations(...commands) {
+  let inputNumber = commands.shift();
+
+  for (let command of commands) {
+    if (command === "chop") {
+      inputNumber /= 2;
+    } else if (command === "dice") {
+      inputNumber = Math.sqrt(inputNumber);
+    } else if (command === "spice") {
+      inputNumber += 1;
+    } else if (command === "bake") {
+      inputNumber *= 3;
+    } else if (command === "fillet") {
+      inputNumber -= inputNumber * 0.2;
+    }
+    console.log(inputNumber);
+  }
+}
+
+// calculateOperations("32", "chop", "chop", "chop", "chop", "chop");
+calculateOperations("9", "dice", "spice", "chop", "bake", "fillet");
