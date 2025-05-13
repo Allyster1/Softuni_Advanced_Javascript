542ab655f0ab60bcba9dc98520e24e631050806a
Allyster <ovardovslav@gmail.com>
2025-05-13 18:35:58 +0300

    Create 03. Largest-Number.js

---
+++ 01-Syntax-Functions-and-Statements/03. Largest-Number.js
@@ -0,0 +1,6 @@
+function findBiggestNumber(first, second, third) {
+  const highestNumber = Math.max(first, second, third);
+  console.log(`The largest number is ${highestNumber}.`);
+}
+
+findBiggestNumber(5, -3, 16);
