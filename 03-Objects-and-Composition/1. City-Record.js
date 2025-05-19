00bbd3936b4fdddb5dd915b799d5d61bc3916bba
Allyster <ovardovslav@gmail.com>
2025-05-19 23:19:49 +0300

    Create 1. City-Record.js

---
+++ 03-Objects-and-Composition/1. City-Record.js
@@ -0,0 +1,11 @@
+function printRecord(name, population, treasure) {
+  const record = {
+    name: name,
+    population: population,
+    treasury: treasure,
+  };
+
+  return record;
+}
+
+printRecord("Tortuga", 7000, 15000);
