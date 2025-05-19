50a01e1e07e4dc9bdab8d3f57f5a95b0de77ee23
Allyster <ovardovslav@gmail.com>
2025-05-19 19:48:56 +0300

    Update 05. Extract-Increasing-Subset-from-Array.js

--- 02-Arrays and Nested Arrays - Exercise/05. Extract-Increasing-Subset-from-Array.js
+++ 02-Arrays and Nested Arrays - Exercise/05. Extract-Increasing-Subset-from-Array.js
@@ -1,10 +1,13 @@
 function printNumbers(array) {
-  return array.reduce((result, current) => {
-    if (result.length === 0 || current >= result[result.length - 1]) {
-      result.push(current);
+  const result = array.reduce((acc, current) => {
+    if (acc.length === 0 || current >= acc[acc.length - 1]) {
+      acc.push(current);
     }
-    return result;
+    return acc;
   }, []);
+
+  //return result;
+  console.log(result);
 }
 
 printNumbers([1, 3, 8, 4, 10, 12, 3, 2, 24]);
