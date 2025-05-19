6f3511d74b5a0733cea5c3093e077e6e4324f4b6
Allyster <ovardovslav@gmail.com>
2025-05-19 20:01:57 +0300

    Update 06. List-of-Names.js

--- 02-Arrays and Nested Arrays - Exercise/06. List-of-Names.js
+++ 02-Arrays and Nested Arrays - Exercise/06. List-of-Names.js
@@ -1,11 +1,9 @@
 function sortNames(array) {
-  const sorted = array.sort((a, b) => a.localeCompare(b));
-  counter = 1;
-
-  sorted.forEach((element) => {
-    console.log(`${counter}.${element}`);
-    counter++;
-  });
+  const sorted = array
+    .sort((a, b) => a.localeCompare(b))
+    .forEach((element, index) => {
+      console.log(`${(index, 1)}.${element}`);
+    });
 }
 
 sortNames(["John", "Bob", "Christina", "Ema"]);
