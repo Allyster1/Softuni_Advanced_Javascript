ffb2c95e1fbd9a7e1ebb5c5e028e8c2bd35dd284
Allyster <ovardovslav@gmail.com>
2025-05-20 19:49:10 +0300

    Update 04. Object-Factory.js

--- 03-Objects-and-Composition/04. Object-Factory.js
+++ 03-Objects-and-Composition/04. Object-Factory.js
@@ -1,7 +1,6 @@
 function factory(library, orders) {
   const result = [];
   for (let order of orders) {
-    // Create a copy of the 'template' object from the current order
     const current = Object.assign({}, order.template);
     for (let part of order.parts) {
       current[part] = library[part];
