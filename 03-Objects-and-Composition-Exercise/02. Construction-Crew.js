283455c26905eb925938b17728c9f3e81201d659
Allyster <ovardovslav@gmail.com>
2025-05-20 19:49:19 +0300

    Create 02. Construction-Crew.js

---
+++ 03-Objects-and-Composition-Exercise/02. Construction-Crew.js
@@ -0,0 +1,18 @@
+function hydrateWorker(workerData) {
+  let worker = {
+    ...workerData,
+    levelOfHydrated: workerData.dizziness
+      ? workerData.weight * 0.1 * workerData.experience
+      : workerData.levelOfHydrated,
+    dizziness: false,
+  };
+
+  console.log(worker);
+}
+
+hydrateWorker({
+  weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true,
+});
