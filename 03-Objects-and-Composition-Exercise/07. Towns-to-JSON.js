e43642233a06b6cca9be37798a5288d0871bd35b
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-21 12:43:53 +0300

    Create 07. Towns-to-JSON.js

---
+++ 03-Objects-and-Composition-Exercise/07. Towns-to-JSON.js
@@ -0,0 +1,33 @@
+function printData(arrayData) {
+  let copyData = JSON.parse(JSON.stringify(arrayData));
+
+  let cleanedData = copyData.map((element) =>
+    element
+      .split("|")
+      .map((item) => item.trim())
+      .filter((item) => item !== "")
+  );
+
+  let townInfo = {};
+  let townKeys = cleanedData[0];
+
+  townInfo.data = cleanedData.slice(1).map((row) => {
+    let obj = {};
+    townKeys.forEach((key, i) => {
+      let value = row[i];
+      if (key === "Latitude" || key === "Longitude") {
+        value = Number(parseFloat(value).toFixed(2));
+      }
+      obj[key] = value;
+    });
+    return obj;
+  });
+
+  console.table(JSON.stringify(townInfo.data));
+}
+
+printData([
+  "| Town | Latitude | Longitude |",
+  "| Sofia | 42.696552 | 23.32601 |",
+  "| Beijing | 39.913818 | 116.363625 |",
+]);
