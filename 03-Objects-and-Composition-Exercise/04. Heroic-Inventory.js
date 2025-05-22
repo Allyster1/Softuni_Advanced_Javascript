ab9e9c14fc8f7af938217fd9e86876a9bbc737b4
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-22 13:41:29 +0300

    Create 04. Heroic-Inventory.js

---
+++ 03-Objects-and-Composition-Exercise/04. Heroic-Inventory.js
@@ -0,0 +1,19 @@
+function printHeroInventory(arrayData) {
+  let heroInventory = [];
+
+  for (const element of arrayData) {
+    let [name, level, items] = element.split(" / ");
+    level = Number(level);
+
+    items = items ? items.split(", ") : [];
+    heroInventory.push({ name, level, items });
+  }
+
+  return JSON.stringify(heroInventory);
+}
+
+printHeroInventory([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
