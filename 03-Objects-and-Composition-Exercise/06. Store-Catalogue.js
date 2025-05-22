6d2c81e3930c0e85730170cab82c96afeb1a2183
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-22 21:12:33 +0300

    Create 06. Store-Catalogue.js

---
+++ 03-Objects-and-Composition-Exercise/06. Store-Catalogue.js
@@ -0,0 +1,32 @@
+function printCatalogue(input) {
+  let products = input
+    .map((element) => {
+      let [name, price] = element.split(" : ");
+      return { name, price: Number(price) };
+    })
+    .sort((a, b) => a.name.localeCompare(b.name));
+
+  let currentGroup = "";
+
+  for (let product of products) {
+    let firstLetter = product.name[0];
+
+    if (firstLetter !== currentGroup) {
+      currentGroup = firstLetter;
+      console.log(currentGroup);
+    }
+
+    console.log(`  ${product.name}: ${product.price}`);
+  }
+}
+
+printCatalogue([
+  "Appricot : 20.4",
+  "Fridge : 1500",
+  "TV : 1499",
+  "Deodorant : 10",
+  "Boiler : 300",
+  "Apple : 1.25",
+  "Anti-Bug Spray : 15",
+  "T-Shirt : 10",
+]);
