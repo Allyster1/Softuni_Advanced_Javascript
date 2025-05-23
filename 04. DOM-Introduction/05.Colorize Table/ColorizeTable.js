cb26b06b05625be4925f00f111e5408f09f500f3
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-23 22:47:36 +0300

    Update ColorizeTable.js

--- 04. DOM-Introduction/05.Colorize Table/ColorizeTable.js
+++ 04. DOM-Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+  const trRef = document.getElementsByTagName("tr");
+
+  for (let i = 1; i < trRef.length; i += 2) {
+    trRef[i].style.backgroundColor = "teal";
+  }
 }
