fec0768232c7b2dd6f96fd194ea2c4735ab3bd47
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-23 22:38:00 +0300

    Update SumNumbers.js

--- 04. DOM-Introduction/03.Sum Numbers/SumNumbers.js
+++ 04. DOM-Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  const firstNumRef = document.getElementById("num1");
+  const secondNumRef = document.getElementById("num2");
+  let sumRef = document.getElementById("sum");
+
+  sumRef.value = Number(firstNumRef.value) + Number(secondNumRef.value);
 }
