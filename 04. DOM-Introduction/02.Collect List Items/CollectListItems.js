903b2bc9e002d7e997305ba2ef7b65c7a4b9bad5
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-23 22:30:39 +0300

    Update CollectListItems.js

--- 04. DOM-Introduction/02.Collect List Items/CollectListItems.js
+++ 04. DOM-Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+  const ulRef = document.getElementById("items").children;
+  const textAreaRef = document.getElementById("result");
+  const result = [];
+
+  for (const li of ulRef) {
+    console.log(li.textContent);
+    result.push(li.textContent);
+  }
+
+  textAreaRef.textContent = result;
 }
