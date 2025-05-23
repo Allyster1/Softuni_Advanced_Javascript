d177b2b891e9e4a37657eff8b738db3ce3b0cac9
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-23 22:40:14 +0300

    Update ShowMore.js

--- 04. DOM-Introduction/04.Show More/ShowMore.js
+++ 04. DOM-Introduction/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+  const moreRef = document.getElementById("more");
+  const textRef = document.getElementById("text");
+
+  moreRef.style.display = "none";
+  textRef.style.display = "inline";
 }
