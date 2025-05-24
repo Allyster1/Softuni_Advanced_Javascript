ea6d6cb10039b746a4983461c46f86015cf007df
Allyster <ovardovslav@gmail.com>
2025-05-24 20:44:30 +0300

    Update solution.js

--- 04-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ 04-DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,35 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  const searchBtnRef = document.getElementById("searchBtn");
+  const searchFieldRef = document.getElementById("searchField");
+  const trRef = document.getElementsByTagName("tbody")[0].children;
 
-   function onClick() {
-      //   TODO:
+  searchBtnRef.addEventListener("click", searchData);
 
-   }
+  function searchData() {
+    const searchText = searchFieldRef.value.trim().toLowerCase();
+
+    for (let item of trRef) {
+      item.classList.remove("select");
+    }
+
+    if (!searchText) {
+      console.log("returned");
+      return;
+    }
+
+    for (let item of trRef) {
+      const tdElements = item.getElementsByTagName("td");
+
+      for (let td of tdElements) {
+        const tdText = td.textContent.toLowerCase();
+
+        if (tdText.includes(searchText)) {
+          item.classList.add("select");
+          break;
+        }
+      }
+    }
+
+    searchFieldRef.value = "";
+  }
 }
