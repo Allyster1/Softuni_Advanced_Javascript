689f17a660be56b125eb6f12aa71aa67eee45f8e
Allyster <ovardovslav@gmail.com>
2025-05-25 01:18:37 +0300

    Update solution.js

--- 04-DOM-Introduction-Exercise/09. Number Convertor/solution.js
+++ 04-DOM-Introduction-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+  const conversionType = document.getElementById("selectMenuTo");
+  const inputNumber = document.getElementById("input");
+  const result = document.getElementById("result");
+  const btn = document.querySelector("button");
 
-    //TODO...
+  if (conversionType.options.length <= 1) {
+    const binaryOption = document.getElementsByTagName("option")[1];
+    binaryOption.textContent = "Binary";
+    binaryOption.value = "binary";
+
+    const hexadecimalOption = document.createElement("option");
+    hexadecimalOption.textContent = "Hexadecimal";
+    hexadecimalOption.value = "hexadecimal";
+
+    conversionType.appendChild(hexadecimalOption);
+  }
+
+  btn.addEventListener("click", () => {
+    const number = Number(inputNumber.value);
+
+    if (conversionType.value === "binary") {
+      result.value = number.toString(2);
+    } else if (conversionType.value === "hexadecimal") {
+      result.value = number.toString(16).toUpperCase();
+    }
+    inputNumber.value = "";
+  });
 }
