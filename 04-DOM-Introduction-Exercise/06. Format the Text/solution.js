db59549c16bd00cbdaaf9fc8d3e9ac97d2b07962
Allyster <ovardovslav@gmail.com>
2025-05-24 23:37:16 +0300

    Update solution.js

--- 04-DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ 04-DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,56 @@
 function solve() {
-  //TODO
+  const textAreaRef = document.getElementById("input");
+  const output = document.getElementById("output");
+
+  let formattedText = textAreaRef.value
+    .split(".")
+    .map((text) => text.trim())
+    .filter((text) => text);
+
+  let textCounter = formattedText.length;
+  let counter = 0;
+  if (textCounter <= 2) {
+    let text = [];
+    for (let element of formattedText) {
+      text.push(element);
+    }
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text.join(". ") + ".";
+    output.appendChild(paragraph);
+    text = [];
+  } else if (textCounter >= 7) {
+    let text = [];
+    for (let element of formattedText) {
+      if (counter === 3) {
+        counter = 0;
+        const paragraph = document.createElement("p");
+        paragraph.textContent = text.join(". ") + ".";
+        output.appendChild(paragraph);
+        text = [];
+      }
+      text.push(element);
+      counter++;
+    }
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text.join(". ") + ".";
+    output.appendChild(paragraph);
+    text = [];
+  } else {
+    let text = [];
+    for (let element of formattedText) {
+      if (counter === 3) {
+        counter = 0;
+        const paragraph = document.createElement("p");
+        paragraph.textContent = text.join(". ") + ".";
+        output.appendChild(paragraph);
+        text = [];
+      }
+      text.push(element);
+      counter++;
+    }
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text.join(". ") + ".";
+    output.appendChild(paragraph);
+    text = [];
+  }
 }
