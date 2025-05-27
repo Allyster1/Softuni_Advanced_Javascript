851bf65599f47857a9f368e78a212e7bc5fa41a4
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-27 20:19:48 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/05. Highlight-Active/app.js
+++ 05-DOM-Manipulation-and-Events/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+  const divElements = document.querySelectorAll("div div");
+
+  for (const div of divElements) {
+    const inputElement = div.querySelector("input");
+
+    if (inputElement) {
+      inputElement.addEventListener("focus", () => {
+        div.classList.add("focused");
+      });
+      inputElement.addEventListener("blur", () => {
+        div.classList.remove("focused");
+      });
+    }
+  }
 }
