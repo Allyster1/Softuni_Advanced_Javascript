713880f322e2b47e5d94e0ba34d4a17f9efac665
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-27 19:55:45 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/02. Delete-From-Table/app.js
+++ 05-DOM-Manipulation-and-Events/02. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const trRef = document.querySelectorAll("tbody tr");
+  const changeType = document.getElementById("result");
+  const inputText = document.querySelector("input");
+  let foundEmail = false;
+
+  if (!inputText.value && inputText.trim() !== "") {
+    return;
+  }
+
+  for (const tr of trRef) {
+    const tdEmail = tr.getElementsByTagName("td")[1];
+    if (tdEmail && tdEmail.textContent === inputText.value) {
+      tr.remove();
+      foundEmail = true;
+      break;
+    }
+  }
+
+  changeType.textContent = foundEmail ? "Deleted." : "Not found.";
+  inputText.value = "";
 }
