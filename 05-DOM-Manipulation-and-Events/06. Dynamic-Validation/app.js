223778b2950db651ca7c0c3e9189de558c379bf0
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-27 21:35:08 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/06. Dynamic-Validation/app.js
+++ 05-DOM-Manipulation-and-Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+  const emailRef = document.getElementById("email");
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  emailRef.addEventListener("change", () => {
+    const email = emailRef.value;
+    if (emailRegex.test(email)) {
+      emailRef.classList.remove("error");
+    } else {
+      emailRef.classList.add("error");
+    }
+  });
 }
