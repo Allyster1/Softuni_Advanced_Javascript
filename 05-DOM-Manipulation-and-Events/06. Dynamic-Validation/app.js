6ecb7404685c9f2ad377144013b2dcf53a82f451
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-28 21:13:01 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/06. Dynamic-Validation/app.js
+++ 05-DOM-Manipulation-and-Events/06. Dynamic-Validation/app.js
@@ -1,10 +1,9 @@
 function validate() {
   const emailRef = document.getElementById("email");
-  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
   emailRef.addEventListener("change", () => {
-    const email = emailRef.value;
-    if (emailRegex.test(email)) {
+    if (pattern.test(emailRef.value)) {
       emailRef.classList.remove("error");
     } else {
       emailRef.classList.add("error");
