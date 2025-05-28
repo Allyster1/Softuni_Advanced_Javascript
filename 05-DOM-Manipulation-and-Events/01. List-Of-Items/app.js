b05e64c2bb33a0c06ae28bcbc0068945d9716c55
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-28 19:27:51 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/01. List-Of-Items/app.js
+++ 05-DOM-Manipulation-and-Events/01. List-Of-Items/app.js
@@ -1,11 +1,11 @@
 function addItem() {
-  const ulItems = document.getElementById("items");
   const newItemText = document.getElementById("newItemText");
 
   const li = document.createElement("li");
-
   li.textContent = newItemText.value;
 
+  const ulItems = document.getElementById("items");
   ulItems.appendChild(li);
+
   newItemText.value = "";
 }
