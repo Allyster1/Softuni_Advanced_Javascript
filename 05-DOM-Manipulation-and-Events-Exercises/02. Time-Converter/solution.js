1a71565592cb4f70c93ac732d5e61ef58f7f29bd
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-28 23:20:22 +0300

    Update solution.js

--- 05-DOM-Manipulation-and-Events-Exercises/02. Time-Converter/solution.js
+++ 05-DOM-Manipulation-and-Events-Exercises/02. Time-Converter/solution.js
@@ -1,4 +1,30 @@
 function attachEventsListeners() {
+  const inputs = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    seconds: document.getElementById("seconds"),
+  };
 
-    console.log('TODO:...');
+  const ratios = {
+    days: 1,
+    hours: 24,
+    minutes: 1440,
+    seconds: 86400,
+  };
+
+  const convert = (value, unit) => {
+    const days = value / ratios[unit];
+    for (const key in inputs) {
+      inputs[key].value = days * ratios[key];
+    }
+  };
+
+  document.querySelector("main").addEventListener("click", (e) => {
+    if (e.target.type === "button") {
+      const unit = e.target.previousElementSibling.id;
+      const value = Number(inputs[unit].value);
+      convert(value, unit);
+    }
+  });
 }
