f82728b57bc9626213cf08dedb8123ef590b493a
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-28 20:31:39 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events/03. Add-Delete/app.js
+++ 05-DOM-Manipulation-and-Events/03. Add-Delete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+  const newItemText = document.getElementById("newItemText");
+
+  if (!newItemText.value) {
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = newItemText.value;
+
+  const deleteBtn = document.createElement("a");
+  deleteBtn.textContent = "[Delete]";
+  deleteBtn.href = "#";
+  li.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+    // event.target.parentElement.remove();
+  });
+
+  const ulItems = document.getElementById("items");
+  ulItems.appendChild(li);
+
+  newItemText.value = "";
 }
