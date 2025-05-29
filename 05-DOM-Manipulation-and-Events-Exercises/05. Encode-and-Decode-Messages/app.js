beb17366bfbf56fc65ef317c2817247c820ceb31
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-29 12:52:15 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events-Exercises/05. Encode-and-Decode-Messages/app.js
+++ 05-DOM-Manipulation-and-Events-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const [encodeBtn, decodeBtn] = document.querySelectorAll("button");
+  const [encodeText, decodeText] = document.querySelectorAll("textarea");
+
+  encodeBtn.addEventListener("click", () => {
+    const text = encodeText.value;
+    if (!text) return;
+
+    let encodedMsg = "";
+    text.split("").forEach((char) => {
+      encodedMsg += String.fromCharCode(char.charCodeAt(0) + 1);
+    });
+
+    decodeText.value = encodedMsg;
+    encodeText.value = "";
+
+    decodeBtn.disabled = false;
+  });
+
+  decodeBtn.addEventListener("click", () => {
+    const text = decodeText.value;
+    if (!text) return;
+
+    let decodedMsg = "";
+    text.split("").forEach((char) => {
+      decodedMsg += String.fromCharCode(char.charCodeAt(0) - 1);
+    });
+
+    decodeText.value = decodedMsg;
+
+    decodeBtn.disabled = true;
+  });
 }
