c187cf915aa4006db119b7bcf27aa6e11920af13
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-29 11:06:45 +0300

    Update app.js

--- 05-DOM-Manipulation-and-Events-Exercises/03. Locked-Profile/app.js
+++ 05-DOM-Manipulation-and-Events-Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const profilesInfo = Array.from(document.querySelectorAll(".profile"));
+
+  for (const profile of profilesInfo) {
+    const [lock, unlock] = profile.querySelectorAll('input[type="radio"]');
+
+    const button = profile.querySelector("button");
+    const hiddenInfo = profile.querySelector("div");
+
+    button.addEventListener("click", () => {
+      if (unlock.checked) {
+        if (hiddenInfo.style.display === "inline-block") {
+          hiddenInfo.style.display = "none";
+          button.textContent = "Show more";
+        } else {
+          hiddenInfo.style.display = "inline-block";
+          button.textContent = "Hide it";
+        }
+      }
+    });
+  }
 }
