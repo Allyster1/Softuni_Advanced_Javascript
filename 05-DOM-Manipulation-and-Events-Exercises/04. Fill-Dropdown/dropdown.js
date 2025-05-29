e17549795351440205db33d190261522b1475010
Slavi Ovardov <ovardovslav@gmail.com>
2025-05-29 12:23:10 +0300

    Update dropdown.js

--- 05-DOM-Manipulation-and-Events-Exercises/04. Fill-Dropdown/dropdown.js
+++ 05-DOM-Manipulation-and-Events-Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+  const menu = document.getElementById("menu");
+  const input = document.getElementById("newItemText");
+  const inputValue = document.getElementById("newItemValue");
+  if (!input.value || !inputValue.value) {
+    return;
+  }
+
+  option = document.createElement("option");
+  option.textContent = input.value;
+  option.value = inputValue.value;
+
+  menu.appendChild(option);
+  inputValue.value = "";
+  input.value = "";
 }
