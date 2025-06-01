f34940405831b7f8849a8ecb5f5305605b96e9b6
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-01 22:18:16 +0300

    Update 01. Area-and-Volume-Calculator.js

--- 06-Advanced-Functions/01. Area-and-Volume-Calculator.js
+++ 06-Advanced-Functions/01. Area-and-Volume-Calculator.js
@@ -1,27 +1,29 @@
-function calculate(area, vol, dataJson) {
-  let data = JSON.parse(dataJson);
-  let result = [];
+function solve(area, vol, input) {
+  const dataInput = JSON.parse(input);
+  const result = [];
 
-  for (const element of data) {
+  for (const data of dataInput) {
+    const areaValue = area.call(data);
+    const volValue = vol.call(data);
     result.push({
-      area: area.call(areaFunc),
-      volume: volume.call(volFunc),
+      area: areaValue,
+      volume: volValue,
     });
   }
 
   return result;
 }
 
-calculate(
-  areaFunc,
-  volFunc,
-  `[ {"x":"1","y":"2","z":"10"}, {"x":"7","y":"7","z":"10"}, {"x":"5","y":"2","z":"10"} ]`
-);
-
-function areaFunc() {
+function area() {
   return Math.abs(this.x * this.y);
 }
 
-function volFunc() {
+function vol() {
   return Math.abs(this.x * this.y * this.z);
 }
+
+solve(
+  area,
+  vol,
+  `[ {"x":"1","y":"2","z":"10"}, {"x":"7","y":"7","z":"10"}, {"x":"5","y":"2","z":"10"} ]`
+);
