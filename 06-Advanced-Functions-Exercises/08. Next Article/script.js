010fe2e97bcdd8c6535bb260b9a7b208577c86e4
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-02 23:01:19 +0300

    Update script.js

--- 06-Advanced-Functions-Exercises/08. Next Article/script.js
+++ 06-Advanced-Functions-Exercises/08. Next Article/script.js
@@ -1,3 +1,24 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(arrayInput) {
+  const contentRef = document.getElementById("content");
+  const counterLimit = arrayInput.length;
+  let counter = 0;
+
+  return showNext;
+
+  function showNext() {
+    if (counter >= counterLimit) {
+      return;
+    }
+    createArticle(arrayInput[counter]);
+    counter += 1;
+  }
+
+  function createArticle() {
+    const article = document.createElement("article");
+    const p = document.createElement("p");
+    p.textContent = arrayInput[counter];
+
+    article.appendChild(p);
+    contentRef.appendChild(article);
+  }
 }
