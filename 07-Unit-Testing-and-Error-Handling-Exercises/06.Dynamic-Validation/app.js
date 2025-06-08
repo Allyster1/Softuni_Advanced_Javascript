bccf9f583b26762e1d5d276f82b5127a4d30f6a9
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-08 20:03:03 +0300

    Update app.js

--- 07-Unit-Testing-and-Error-Handling-Exercises/06.Dynamic-Validation/app.js
+++ 07-Unit-Testing-and-Error-Handling-Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+  const input = document.getElementById("email");
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g;
+
+  input.addEventListener("change", () => {
+    const value = input.value;
+
+    if (emailPattern.test(value)) {
+      input.classList.remove("error");
+    } else {
+      input.classList.add("error");
+    }
+  });
 }
