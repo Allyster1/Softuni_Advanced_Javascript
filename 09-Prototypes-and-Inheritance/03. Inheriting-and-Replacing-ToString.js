2e92064b7016d3061b3dd3c52b30fa21a1a3e417
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-12 00:57:24 +0300

    removing dynamic usage of method toString()

--- 09-Prototypes-and-Inheritance/03. Inheriting-and-Replacing-ToString.js
+++ 09-Prototypes-and-Inheritance/03. Inheriting-and-Replacing-ToString.js
@@ -1,40 +1,37 @@
 function toStringExtension() {
   class Person {
-    name;
-    email;
     constructor(name, email) {
       this.name = name;
       this.email = email;
     }
 
     toString() {
-      return `${this.constructor.name} (name: ${this.name}, email: ${this.email}`;
+      return `Person (name: ${this.name}, email: ${this.email})`;
     }
   }
 
   class Student extends Person {
-    course;
     constructor(name, email, course) {
       super(name, email);
       this.course = course;
     }
 
     toString() {
-      return `${super.toString()}, course: ${this.course})`;
+      return `Student (name: ${this.name}, email: ${this.email}, course: ${this.course})`;
     }
   }
 
   class Teacher extends Person {
-    subject;
     constructor(name, email, subject) {
       super(name, email);
       this.subject = subject;
     }
 
     toString() {
-      return `${super.toString()}, subject: ${this.subject})`;
+      return `Teacher (name: ${this.name}, email: ${this.email}, subject: ${this.subject})`;
     }
   }
 
   return { Person, Teacher, Student };
 }
+toStringExtension();
