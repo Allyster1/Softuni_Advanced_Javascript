8e19a3c63861a69418e3320ff9d93a011736439b
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-12 12:20:21 +0300

    adding solution for task 4

--- 09-Prototypes-and-Inheritance/04. Extend-Prototype.js
+++ 09-Prototypes-and-Inheritance/04. Extend-Prototype.js
@@ -1,3 +1,7 @@
 function extendPrototype(classToExtend) {
-  // TODO:
+  classToExtend.prototype.species = "Human";
+
+  classToExtend.prototype.toSpeciesString = function () {
+    return `I am a ${this.species}. ${this.toString()}`;
+  };
 }
