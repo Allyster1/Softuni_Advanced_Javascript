05bc2c05064b2fd4e6643a0ffbe805cedd4c0c19
Slavi Ovardov <ovardovslav@gmail.com>
2025-06-12 12:13:24 +0300

    adding solution for task 5

--- 09-Prototypes-and-Inheritance/05. Class-Hierarch.js
+++ 09-Prototypes-and-Inheritance/05. Class-Hierarch.js
@@ -1,24 +1,26 @@
 function calculateFigure() {
   class Figure {
-    units;
-    constructor(units) {
-      if (typeof units !== "string" || !["mm", "m"].includes(units)) {
+    constructor(units = "cm") {
+      if (!["mm", "cm", "m"].includes(units)) {
         units = "cm";
       }
-
       this.units = units;
     }
 
-    get area() {
-      return this;
+    convertValue(value) {
+      switch (this.units) {
+        case "mm":
+          return value * 10;
+        case "cm":
+          return value;
+        case "m":
+          return value / 100;
+      }
     }
 
-    set changeUnits(value) {
-      if (!["cm", "m", "mm"].includes(value)) {
-        return;
-      }
-
-      if (this.units === "mm") {
+    changeUnits(newUnit) {
+      if (["mm", "cm", "m"].includes(newUnit)) {
+        this.units = newUnit;
       }
     }
 
@@ -28,14 +30,18 @@ function calculateFigure() {
   }
 
   class Circle extends Figure {
-    radius;
     constructor(radius, units) {
       super(units);
-      this.radius = radius;
+      this._radius = radius;
+    }
+
+    get radius() {
+      return this.convertValue(this._radius);
     }
 
     get area() {
-      return Math.PI * this.radius * this.radius;
+      const r = this.radius;
+      return Math.PI * r * r;
     }
 
     toString() {
@@ -44,41 +50,44 @@ function calculateFigure() {
   }
 
   class Rectangle extends Figure {
-    width;
-    height;
-    units;
     constructor(width, height, units) {
       super(units);
-      this.width = width;
-      this.height = height;
-      this.units = units;
+      this._width = width;
+      this._height = height;
+    }
+
+    get width() {
+      return this.convertValue(this._width);
+    }
+
+    get height() {
+      return this.convertValue(this._height);
     }
 
     get area() {
       return this.width * this.height;
     }
+
     toString() {
       return `Figures units: ${this.units} Area: ${this.area} - width: ${this.width}, height: ${this.height}`;
     }
   }
 
-  let r = new Rectangle(3, 4, "mm");
-  console.log(r);
-
-  console.log(r.area); // 1200
-  console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
-
   return { Figure, Circle, Rectangle };
 }
 
-calculateFigure();
+const { Circle, Rectangle } = calculateFigure();
 
-// let c = new Circle(5);
-// console.log(c.area); // 78.53981633974483
-// console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+let c = new Circle(5);
+console.log(c.area); // 78.53981633974483
+console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+c.changeUnits("mm");
+console.log(c.area); // 7853.981633974483
+console.log(c.toString()); // Figures units: mm Area: 7853.981633974483 - radius: 50
 
-// let r = new Rectangle(3, 4, "mm");
-// console.log(r.area); // 1200
-// console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
-// r.changeUnits("cm");
-// console.log(r.area); // 12
+let r = new Rectangle(3, 4, "mm");
+console.log(r.area); // 1200
+console.log(r.toString()); // Figures units: mm Area: 1200 - width: 30, height: 40
+r.changeUnits("cm");
+console.log(r.area); // 12
+console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4
